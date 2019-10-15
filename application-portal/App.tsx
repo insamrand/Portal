d5071b1125413f0bbb0f6b74d234b965aa5fc46d
Phusitkarn Insamrand <phusitkarn_i@banpu.co.th>
2019-10-15 13:42:40 +0700

    update

--- application-portal/App.tsx
+++ application-portal/App.tsx
@@ -1,19 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { Text, View, Image, StyleSheet } from "react-native";
+import { Content, Form, Item, Label, Input } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <View>
+        <Image
+          style={styles.banpuLogo}
+          source={require("./src/assets/images/banpu_logo.png")}
+        />
+        <Content>
+          <Form>
+            <Item stackedLabel>
+              <Label>Username</Label>
+              <Input />
+            </Item>
+            <Item stackedLabel last>
+              <Label>Password</Label>
+              <Input />
+            </Item>
+          </Form>
+        </Content>
+      </View>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  banpuLogo: {
+    width: 42,
+    height: 100,
+    resizeMode: "contain"
+  }
 });
+export default App;
--- application-portal/src/pages/home/HomePage.tsx
+++ application-portal/src/pages/home/HomePage.tsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Text, View } from "react-native";
 
 export class HomePage extends Component {
+  
   render() {
     return (
       <View>
