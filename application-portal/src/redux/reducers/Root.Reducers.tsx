fc55c390e016822aff67aaa25cc0524dddae5c7f
Phusitkarn Insamrand <phusitkarn_i@banpu.co.th>
2019-10-17 15:39:02 +0700

    del reducers login

--- application-portal/src/redux/reducers/Root.Reducers.tsx
+++ application-portal/src/redux/reducers/Root.Reducers.tsx
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
-import LoginReducers from "./Login.Reducers";
 import HomeReducers from "./Home.Reducers";
 import { reducer as FormReducer } from "redux-form";
 
 export default () =>
   combineReducers({
-    login: LoginReducers,
     home: HomeReducers,
     form: FormReducer
   });
